f67d0b2dc58f7e6b7c1c14a5df5c0a689ddedec7
Chamath Jayasena <chamath.jayasena@protonmail.com>
2023-03-25 09:57:13 +0530

    Add check for model file existence

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import pipeline
+import os
+import sys
+
 
 def load_model(model_path):
     # Load model
@@ -9,10 +12,27 @@ def load_model(model_path):
     # Return classifier
     return classifier
 
+
 def load_emotion_distilroberta():
     model_path = "models/emotion-english-distilroberta-base"
-    return load_model(model_path)
+    if model_exists(model_path):
+        return load_model(model_path)
+    else:
+        print("Error: Model file not found at", model_path)
+        sys.exit()
+
 
 def load_suicidal_text_electra():
     model_path = "models/suicidal-text-electra-cj"
-    return load_model(model_path)
+    if model_exists(model_path):
+        return load_model(model_path)
+    else:
+        print("Error: Model file not found at", model_path)
+        sys.exit()
+
+
+def model_exists(path):
+    if os.path.exists(path):
+        return True
+    else:
+        return False
